f597baf4ee0ad78edd8d46f67d05fc86aaafb3d6
Halley Rios <hrios@symplicity.com>
2021-03-29 23:17:25 -0300

    [WIP] feat: Refactor API calls into one service.

---
+++ src/services/TodoService.js
@@ -0,0 +1,10 @@
+class TodoService {
+    static #API_URL = 'https://5df94eace9f79e0014b6afab.mockapi.io/api/v1/tasks';
+    constructor() {}
+
+    getTasks() {
+        return [];
+    }
+}
+
+export default TodoService;
